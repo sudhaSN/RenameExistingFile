d15b4f885acdb0982d0ec4ce363cf615a432df42
sudhaSN <95419061+sudhaSN@users.noreply.github.com>
2023-01-06 18:27:21 +0530

    Add files via upload

---
+++ file_system_move_file.py
@@ -0,0 +1,5 @@
+import os
+source = 'C:/Users/reach/Desktop/Python/project115/main.txt'
+destination = 'C:/Users/reach/Desktop/Python/project115/newFile.txt'
+os.rename(source, destination)
+print('Succesfully Renamed!')
